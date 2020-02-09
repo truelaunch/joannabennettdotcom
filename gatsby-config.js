b78ff0cfa6eb0eea27b2a746cebeb3cbf803d1f9
Joanna Bennett <joanna@mandrill.com>
2020-02-09 13:16:30 -0500

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/images/joanna-bennett-logo.ico`,
       },
     },
     // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
